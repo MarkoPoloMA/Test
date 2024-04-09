10536e31c0309f0ee555604919113ba507ef866b
vologzhanin@ymail.com <vologzhanin@ymail.com>
2024-04-09 19:58:25 +0400

    Add rerandom array

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -20,24 +20,42 @@ void main()
 	{
 		system("cls");
 
+		cout << "1 - Вывести массив" << endl <<
+			 "0 - Выход из массива" << endl <<
+			 "2 - show with indexes" << endl;
 		cout << "1 - Вывести массив" << endl;
 		cout << "2 - Сортировать по возрастанию" << endl;
 		cout << "3 - Сортировать по убыванию" << endl;
 		cout << "0 - Выход из массива" << endl;
+
 
 		int input;
 		cin >> input;
 		if (input == 2)
 			sort(Arrive, Arrive + size);
 
+<<<<<<< .mine
 		if (input == 3)
 			sort(Arrive, Arrive + size, [](int a, int b) { return a > b; });
 
 		if (input == 0)
+=======
+		switch (input) {
+		case(1):
+
+
+>>>>>>> .theirs
 			break;
-		if (input == 1)
+		case(2):
 			for (int i = 0; i < size; i++)
 				cout << Arrive[i] << " ";
-		cout << endl;
+			cout << endl;
+			system("pause");
+		case(3):
+			for (int i = 0; i < size; i++)
+				cout << "[" << i << "] =>" << Arrive[i]  << endl;
+			cout << endl;
+			system("pause");
+		}
 	}
 }
